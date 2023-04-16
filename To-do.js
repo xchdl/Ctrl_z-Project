5131e4e516b23654905c4251743fe08efbc3da2d
Phoebe <126813671+Tiramisus29@users.noreply.github.com>
2023-04-16 00:28:04 -0400

    Update To-do.js

--- To-do.js
+++ To-do.js
@@ -1,55 +1,32 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+document.querySelector('#butn').onclick = function(){
+    if(document.querySelector('#todo input').value.length == 0){
+        alert("Please Enter a Task")
     }
-  }
+    else{
+        document.querySelector('#list').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#todo input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div> `;
+
+        var current_list = document.querySelectorAll(".delete");
+        for(var i=0; i<current_list.length; i++){
+            current_list[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var list = document.querySelectorAll(".task");
+        for(var i=0; i<list.length; i++){
+            list[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#todo input").value = "";
+    }
 }
