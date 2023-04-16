ca378c9bafe0facd319006934f224ac29223f5c6
Abby Shah <126813436+xchdl@users.noreply.github.com>
2023-04-16 01:57:48 -0400

    Delete Welcome Page.js

--- Welcome Page.js
+++
@@ -1,7 +0,0 @@
-function myFunction(x) {
-    document.getElementById("OverlayMenu").style.width = "250px";
-  
-  }
-  function closebtn(){
-    document.getElementById("OverlayMenu").style.width = "0";
-  }
