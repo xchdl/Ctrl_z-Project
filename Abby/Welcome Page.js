d706c77454addd2b030051f405b2b85eb772fa28
Abby Shah <126813436+xchdl@users.noreply.github.com>
2023-04-16 01:55:44 -0400

    Add files via upload

---
+++ Abby/Welcome Page.js
@@ -0,0 +1,7 @@
+function myFunction(x) {
+    document.getElementById("OverlayMenu").style.width = "250px";
+  
+  }
+  function closebtn(){
+    document.getElementById("OverlayMenu").style.width = "0";
+  }
