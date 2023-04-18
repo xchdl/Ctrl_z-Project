14b6ac430f09cceb262d675276da45766fc77de0
tashiyas <126813518+tashiyas@users.noreply.github.com>
2023-04-18 17:35:08 -0400

    Delete Tash_profset.js

--- Tash/Tash_profset.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var passwordInput = document.getElementById('lname1');
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-  } else {
-    passwordInput.type = "password";
-  }
-}
