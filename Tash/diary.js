683bf0dccc5973e8a1e942e51a843df4f9931c9e
Phoebe <126813671+Tiramisus29@users.noreply.github.com>
2023-04-19 18:14:02 -0400

    Update diary.js

--- Tash/diary.js
+++ Tash/diary.js
@@ -15,30 +15,31 @@ addBtn.addEventListener("click", function (e) {
   showStories();
 });
 function showStories() {
-  let stories = localStorage.getItem("stories");
-  if (stories == null) {
-    storiesObj = [];
-  } else {
-    storiesObj = JSON.parse(stories);
-  }
-  let html = "";
-  storiesObj.forEach(function (element, index) {
-    html += `
-              <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
-                      <div class="card-body">
-                          <h5 class="card-title">Story ${index + 1}</h5>
-                          <p class="card-text"> ${element}</p>
-                          <button id="${index}"onclick="deleteStory(this.id)" class="btn btn-primary">Delete Story</button>
-                      </div>
-                  </div>`;
-  });
-  let storiesElm = document.getElementById("stories");
-  if (storiesObj.length != 0) {
-    storiesElm.innerHTML = html;
-  } else {
-    storiesElm.innerHTML = `Nothing to show! Use "Write a story" section above to write your story.`;
-  }
-}
+    let stories = localStorage.getItem("stories");
+    if (stories == null) {
+      storiesObj = [];
+    } else {
+      storiesObj = JSON.parse(stories);
+    }
+    let html = "";
+    storiesObj.forEach(function (element, index) {
+      html += `
+        <div class="note" >
+          <div class="box-body">
+            <h5 class="box-title">Story ${index + 1}</h5>
+            <p class="text"> ${element}</p>
+            <button id="${index}" onclick="editStory(this.id)" class="btn.edit">Edit Story</button>
+            <button id="${index}" onclick="deleteStory(this.id)" class="btn.del">Delete Story</button>
+          </div>
+        </div>`;
+    });
+    let storiesElm = document.getElementById("stories");
+    if (storiesObj.length != 0) {
+      storiesElm.innerHTML = html;
+    } else {
+      storiesElm.innerHTML = `Nothing to show! Use "Write a story" section above to write your story.`;
+    }
+  }  
 
 function deleteStory(index) {
   let stories = localStorage.getItem("stories");
@@ -58,7 +59,7 @@ let search = document.getElementById('searchTxt');
 search.addEventListener("input", function () {
 
   let inputVal = search.value.toLowerCase();
-  let storyCards = document.getElementsByClassName('noteCard');
+  let storyCards = document.getElementsByClassName('note');
   Array.from(storyCards).forEach(function (element) {
     let cardTxt = element.getElementsByTagName("p")[0].innerText;
     if (cardTxt.includes(inputVal)) {
@@ -76,3 +77,17 @@ function myFunction(x) {
   function closebtn(){
     document.getElementById("OverlayMenu").style.width = "0";
   }
+
+  function editStory(index) {
+    let stories = localStorage.getItem("stories");
+    let storiesObj = JSON.parse(stories);
+    let story = storiesObj[index];
+    let editTxt = prompt("Enter the new story", story);
+    if (editTxt === null || editTxt === "") {
+      return;
+    }
+    storiesObj[index] = editTxt;
+    localStorage.setItem("stories", JSON.stringify(storiesObj));
+    showStories();
+  }
+  
