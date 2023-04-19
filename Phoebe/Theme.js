4f4c0f2e022a8ef56d19f43b1772741f5e20ccaa
Phoebe <126813671+Tiramisus29@users.noreply.github.com>
2023-04-19 04:06:43 -0400

    Update Theme.js

--- Phoebe/Theme.js
+++ Phoebe/Theme.js
@@ -16,11 +16,11 @@ for (let i of switches) {
 
 function setTheme(theme) {
   if (theme == 'default') {
-    document.getElementById('switcher-id').href = './Project/Themes/default.css';
+    document.getElementById('switcher-id').href = './Themes/default.css';
   } else if (theme == 'night') {
-    document.getElementById('switcher-id').href = './Project/Themes/night.css';
+    document.getElementById('switcher-id').href = './Themes/night.css';
   } else if (theme == 'comfort') {
-    document.getElementById('switcher-id').href = './Project/Themes/comfort.css';
+    document.getElementById('switcher-id').href = './Themes/comfort.css';
   }
   localStorage.setItem('style', theme);
 }
