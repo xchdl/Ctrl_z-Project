285f32a2499cc71fd26af1060bd585ac69fe1991
Abby Shah <126813436+xchdl@users.noreply.github.com>
2023-04-19 23:17:34 -0400

    Add files via upload

--- Final Project/To-do.js
+++ Final Project/To-do.js
@@ -28,3 +28,10 @@ document.querySelector('#butn').onclick = function(){
         document.querySelector("#todo input").value = "";
     }
 }
+function myFunction(x) {
+    document.getElementById("OverlayMenu").style.width = "250px";
+  
+  }
+  function closebtn(){
+    document.getElementById("OverlayMenu").style.width = "0";
+  }
