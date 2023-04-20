6867b226b094a7f45bdb405911fe01723f5e1a43
Abby Shah <126813436+xchdl@users.noreply.github.com>
2023-04-19 23:07:37 -0400

    Add files via upload

--- Final Project/profset.js
+++ Final Project/profset.js
@@ -7,9 +7,19 @@ function myFunction() {
   }
 
 }
+window.addEventListener('load', function() {
+  const username = localStorage.getItem('username');
+  const password = localStorage.getItem('password');
+  const email = localStorage.getItem('email');
+  document.getElementById('lname').value = username;
+  document.getElementById('lname1').value = password;
+  document.getElementById('lname2').value = email;
+});
 
 
-function myFunction(x) {
+
+
+function myFunction2(x) {
   document.getElementById("OverlayMenu").style.width = "250px";
 
 }
